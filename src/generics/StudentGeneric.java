b0476ce02861a86d19cda117857ddac102570cc0
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-31 21:33:15 +0530

    Initial commit

---
+++ src/generics/StudentGeneric.java
@@ -0,0 +1,28 @@
+package generics;
+
+public class StudentGeneric<T,S>{
+
+    private T rollNumber;
+    private S name;
+
+    public StudentGeneric(T rollNumber, S name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    public T getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(T rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public S getName() {
+        return name;
+    }
+
+    public void setName(S name) {
+        this.name = name;
+    }
+}
---
+++ src/generics/StudentMain.java
@@ -0,0 +1,9 @@
+package generics;
+
+public class StudentMain {
+
+    StudentGeneric<Integer,String> studentGeneric = new StudentGeneric<>(12,"arjun");
+
+    
+
+}
